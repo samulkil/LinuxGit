277e0d9203770ac5f4228d718e5c26d22ab6fcef
samulkil <72770192+samulkil@users.noreply.github.com>
2023-03-02 15:33:45 -0300

    Add files via upload

---
+++ Exerci1/Exerci1.java
@@ -0,0 +1,28 @@
+package coisaslocais2;
+
+public class Exerci1 {
+	
+
+	public static void main(String[] args)
+	{
+		
+		System.out.print("integrado\n ");
+		
+		
+		int x, y, z;
+		
+		System.out.print("escreve x\n ");
+		x = MyIO.readInt();
+		System.out.print("escreve y\n ");
+		y = MyIO.readInt();
+		
+		z = x + y;
+		
+		System.out.print("x + y = " + z);
+	
+	}
+	
+	
+}
+
+
